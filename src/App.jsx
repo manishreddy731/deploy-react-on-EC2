fa1217b37769b3eea8c3ba456bec1a1c5cde55e3
manishreddy731 <your-email@example.com>
2025-06-25 10:21:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>
+        Hello Mr.Reddy
+      </h1>
+      <h2> Definetly not my first react app on AWS EC2</h2>
+      <h3> Deployed by Mr.Reddy</h3>
+    </div>
+    
+  )
+}
+
+export default App
